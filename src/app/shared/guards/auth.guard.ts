cdbe2bea523e122c769416840937b16ad4b70e18
Yecksin <yecksin@gmail.com>
2020-10-23 21:09:34 -0500

    Implementacion de guard

--- src/app/shared/guards/auth.guard.ts
+++ src/app/shared/guards/auth.guard.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
 import { Observable } from 'rxjs';
+import { map, take } from 'rxjs/operators';
 import { AuthService } from '../services/auth.service';
 
 @Injectable({
@@ -10,16 +11,11 @@ export class AuthGuard implements CanActivate {
 
   constructor(private router: Router, private authService: AuthService) {}
 
-  canActivate(
-    route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-   /* const isLogged = this.authService.isLogged();
-    console.log(`Existe una sesión de usaurio? ${isLogged}`);
-    if (!isLogged) {
-      this.router.navigate(['login']);
-      return false;
-    }*/
-    return true;
+  canActivate(): boolean  {
+    if(!this.authService.logueado){
+      this.router.navigate(['/login']);
+    }
+    return this.authService.logueado;
   }
   
 }
